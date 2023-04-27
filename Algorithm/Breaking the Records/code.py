9df8a10c6bcd99c7740d5f1aee825ceebf94d2af
NEERAJ KUMAR <49531334+Kumarbegnier@users.noreply.github.com>
2023-04-27 13:55:30 +0530

    Create code.py

---
+++ Algorithm/Breaking the Records/code.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'breakingRecords' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY scores as parameter.
+#
+
+def breakingRecords(scores):
+    max_score = min_score = scores[0]
+    max_count = min_count = 0
+    
+    for score in scores:
+        if score > max_score:
+            max_score = score
+            max_count += 1
+        elif score < min_score:
+            min_score = score
+            min_count += 1
+    
+    return [max_count, min_count]
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    scores = list(map(int, input().rstrip().split()))
+
+    result = breakingRecords(scores)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
