51a05148cc4f2c611805b8d1c07e68ae0c919af0
NEERAJ KUMAR <49531334+Kumarbegnier@users.noreply.github.com>
2023-04-27 13:26:09 +0530

    Create code.py

---
+++ Algorithm/Number Line Jumps/code.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    if v1 <= v2:
+        return 'NO'  # The second kangaroo is faster, so they will never meet
+    elif (x2 - x1) % (v1 - v2) == 0:
+        return 'YES'  # They will meet at a whole number of jumps
+    else:
+        return 'NO'  # They won't meet at a whole number of jumps
+
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
