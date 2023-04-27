cdb58881f2fecb4277ccedfb3b473ce92ef9da40
NEERAJ KUMAR <49531334+Kumarbegnier@users.noreply.github.com>
2023-04-27 14:23:56 +0530

    Create code.py

---
+++ Algorithm/Subarray Division/code.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthday' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY s
+#  2. INTEGER d
+#  3. INTEGER m
+#
+
+def birthday(s, d, m):
+    ways = 0
+    window_sum = sum(s[:m])
+    if window_sum == d:
+        ways += 1
+    for i in range(1, len(s) - m + 1):
+        window_sum = window_sum - s[i - 1] + s[i + m - 1]
+        if window_sum == d:
+            ways += 1
+    return ways
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = list(map(int, input().rstrip().split()))
+
+    first_multiple_input = input().rstrip().split()
+
+    d = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    result = birthday(s, d, m)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
