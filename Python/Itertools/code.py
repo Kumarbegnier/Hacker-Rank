edec84a92e839ed91e2e2c0dc971d71d69edc7be
Neeraj Kumar <49531334+Kumarbegnier@users.noreply.github.com>
2023-05-01 18:26:38 +0530

    Delete code.py

--- Python/Itertools/code.py
+++
@@ -1,9 +0,0 @@
-from itertools import product
-
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-
-result = sorted(product(A, B))
-
-for pair in result:
-    print(pair, end=' ')
