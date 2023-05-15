fd39d59951fab6104706845536e7b563bb808719
Neeraj Kumar <49531334+Kumarbegnier@users.noreply.github.com>
2023-05-15 21:35:41 +0530

    Create code.py

---
+++ Python/Math/Mod Divmod/code.py
@@ -0,0 +1,6 @@
+a = int(input())  # First integer
+b = int(input())  # Second integer
+
+print(a // b)  # Integer division: a // b
+print(a % b)  # Modulo operator: a % b
+print(divmod(a, b))  # Divmod of a and b
